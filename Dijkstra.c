26d184067cbc776d90e994c40ca87ffffe96c6f3
Xyp1ce <=ramdev4559@gmail.com>
2026-04-24 09:13:10 -0700

    Estructura nodo, creacion de nodos y creacion de caminos

--- Dijkstra.c
+++ Dijkstra.c
@@ -1,72 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct {
-    char[1] name;   // Estará marcado por una sola letra
-    Path* paths[5]; // Lista de apuntadores
-    int quant;      // Cantidad de caminos actuales
-}Node;
+typedef struct Nodo{
+    char id;             // Identificado por una letra (A, B, C, ..., Z)
+    struct Nodo **salidas;  // Caminos que salen del nodo
+    int *pesos;            // Lista de pesos
+    int cant;           // Cantidad de nodos
+} Nodo;
 
-typedef struct {
-    Node dest;
-    int weight;
-}Path;
-
-Node* searchLowestPathWeight(*Node node);
-Node* createNode(char nm) // nm -> name
-Path* addPath(int wgt, Node dst); // wgt -> weight | ogn -> origin | dst -> destination
+Nodo* crearNodo(char id); 
+void agregarSalida(Nodo* nodoOriginal, Nodo* nodoSalida, int peso);
 
 int main() {
-    // Creamos los nodos de nuestro grafo
-    nodeA = createNode('A');
-    nodeB = createNode('B');
-    nodeC = createNode('C');
-    nodeD = createNode('D');
-    nodeE = createNode('E');
-    nodeF = createNode('F');
-    // Asignamos caminos a cada nodo
-    // Nodo A
-    nodeA.paths = addPath(10, NULL, nodeD);
-    nodeA.paths = addPath(50, NULL, nodeB);
-    nodeA.paths = addPath(45, NULL, nodeC);
-    // Nodo B
-    nodeB.paths = addPath(10, nodeB, nodeC);
-    nodeB.paths = addPath(15, nodeB, nodeD);
-    // Nodo C
-    nodeC.paths = addPath(30, nodeC, nodeE);
-    // Nodo D
-    nodeD.paths = addPath(10, nodeD, nodeA);
-    nodeD.paths = addPath(15, nodeD, nodeE);
-    // Nodo E
-    nodeE.paths = addPath();
-    nodeE.paths = addPath();
-    nodeE.paths = addPath();
-    // Nodo F
-    nodeE.paths = addPath();
+    Nodo *nodoA = crearNodo('A');
+    printf("Nodo %c creado\n", nodoA->id);
+    Nodo *nodoB = crearNodo('B');
+    printf("Nodo %c creado\n", nodoB->id);
+    Nodo *nodoC = crearNodo('C');
+    printf("Nodo %c creado\n", nodoC->id);
+    Nodo *nodoD = crearNodo('D');
+    printf("Nodo %c creado\n", nodoD->id);
+
+    agregarSalida(nodoA, nodoB, 50);
+    printf("Salida de nodo A a nodo B creada\n");
+    agregarSalida(nodoA, nodoC, 45);
+    printf("Salida de nodo A a nodo C creada\n");
+    agregarSalida(nodoA, nodoD, 10);
+    printf("Salida de nodo A a nodo D creada\n");
     return 0;
 }
 
-Node* createNode(char nm) {
-    // Reservamos memoria para un nuevo nodo
-    newNode = (Node*)calloc(1, sizeof(Path));
-    // Asignamos nombre
-    newNode.name = nm;
-    // Retornar una referencia al nuevo nodo
-    return newNode;
+Nodo* crearNodo(char id) {
+    Nodo *nuevoNodo = (Nodo*) calloc(1, sizeof(Nodo));      // Asignacion de memoria para nuevo nodo
+    nuevoNodo->id = id;                                     // Asiganar identificador al nodo
+    nuevoNodo->salidas = NULL;                              // Creacion de arreglo tamaño 0 de manera predeterminada
+    nuevoNodo->pesos = NULL;                                // Creacion de arreglo tamaño 0 de manera predeterminada
+    nuevoNodo->cant = 0;                                    // Inicializar contador de nodos salientes
+
+    return nuevoNodo;                                       // Retornar direccion del nuevo nodo
 }
 
-Path* addPath(int wgt, Node dst) {
-    // Reservamos memoria para un nuevo camino
-    newPath = (Path*)calloc(1, sizeof(Path));
-    // Crear copia de la cantidad de caminos 
-     int index = ogn.quant;
-    // Asignamos valores
-    newPath.weight = wgt;
-    newPath.dest = dst;
-    // Poner camino en la lista
-    ogn.paths[index] = newPath;
-    // Incrementar la cantidad de caminos actuales
-    ogn.quant++;
-    // Retornamos referencia al nuevo camino
-    return newPath;
+void agregarSalida(Nodo* nodoOriginal, Nodo* nodoSalida, int peso) {
+    nodoOriginal->cant++;
+    int indice = nodoOriginal->cant;                      // Incrementar la cantidad de nodos que hay de salida
+    
+    Nodo **tempSalidas = (Nodo**) realloc(nodoOriginal->salidas, indice * sizeof(Nodo*));
+    if(!tempSalidas) {
+        printf("Error, Fallo al asignar memoria para las salidas. \n");
+        exit(1);
+    }
+    nodoOriginal->salidas = tempSalidas;
+    nodoOriginal->salidas[indice-1] = nodoSalida;
+
+    int *tempPesos = (int*) realloc(nodoOriginal->pesos, indice * sizeof(int));
+    if(!tempPesos) {
+        printf("Error, Fallo al asignar memoria para las salidas. \n");
+        exit(1);
+    }
+    nodoOriginal->pesos = tempPesos;
+    nodoOriginal->pesos[indice-1] = peso;
 }
+
