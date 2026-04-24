7dc9092cb00e2592320c06462c7a49f23a9bb36c
Xyp1ce <=ramdev4559@gmail.com>
2026-04-24 13:46:38 -0700

    Mapeo del grafo

--- Dijkstra.c
+++ Dijkstra.c
@@ -13,24 +13,82 @@ void agregarConexionUnidireccional(Nodo* nodoOriginal, Nodo* nodoSalida, int pes
 void conectarNodos(Nodo* nodoA, Nodo* nodoB, int peso);
 
 int main() {
+    printf("--- Creando nodos ---\n");
     Nodo *nodoA = crearNodo('A');
-    printf("Nodo %c creado\n", nodoA->id);
     Nodo *nodoB = crearNodo('B');
-    printf("Nodo %c creado\n", nodoB->id);
     Nodo *nodoC = crearNodo('C');
-    printf("Nodo %c creado\n", nodoC->id);
     Nodo *nodoD = crearNodo('D');
-    printf("Nodo %c creado\n", nodoD->id);
+    Nodo *nodoE = crearNodo('E');
+    Nodo *nodoF = crearNodo('F');
+    Nodo *nodoG = crearNodo('G');
+    Nodo *nodoH = crearNodo('H');
+    Nodo *nodoI = crearNodo('I');
+    Nodo *nodoJ = crearNodo('J');
+    Nodo *nodoK = crearNodo('K');
+    Nodo *nodoL = crearNodo('L');
+    Nodo *nodoM = crearNodo('M');
+    Nodo *nodoN = crearNodo('N');
+    Nodo *nodoO = crearNodo('O');
+    Nodo *nodoP = crearNodo('P');
+    Nodo *nodoQ = crearNodo('Q');
+    Nodo *nodoR = crearNodo('R');
+    Nodo *nodoS = crearNodo('S');
+    Nodo *nodoT = crearNodo('T');
+    printf("\n--- Nodos creados ---\n");
 
     printf("\n--- Creando conexiones no dirigidas ---\n");
-    conectarNodos(nodoA, nodoB, 50);
-    printf("Conexion bidireccional entre A y B creada\n");
-    
-    conectarNodos(nodoA, nodoC, 45);
-    printf("Conexion bidireccional entre A y C creada\n");
-    
-    conectarNodos(nodoA, nodoD, 10);
-    printf("Conexion bidireccional entre A y D creada\n");
+    conectarNodos(nodoA, nodoB, 100);
+    conectarNodos(nodoA, nodoD, 30);
+    conectarNodos(nodoA, nodoD, 85);
+
+    conectarNodos(nodoB, nodoC, 100);
+    conectarNodos(nodoB, nodoE, 40);
+    conectarNodos(nodoB, nodoG, 70);
+    conectarNodos(nodoB, nodoF, 45);
+
+    conectarNodos(nodoC, nodoH, 60);
+    conectarNodos(nodoC, nodoG, 55);
+
+    conectarNodos(nodoD, nodoP, 50);
+
+    conectarNodos(nodoE, nodoI, 10);
+
+    conectarNodos(nodoF, nodoJ, 55);
+
+    conectarNodos(nodoG, nodoL, 20);
+    conectarNodos(nodoG, nodoK, 15);
+
+    conectarNodos(nodoH, nodoL, 40);
+    conectarNodos(nodoH, nodoN, 85);
+    conectarNodos(nodoH, nodoO, 70);
+
+    conectarNodos(nodoI, nodoP, 90);
+    conectarNodos(nodoI, nodoJ, 60);
+
+    conectarNodos(nodoJ, nodoK, 80);
+    conectarNodos(nodoJ, nodoQ, 45);
+
+    conectarNodos(nodoK, nodoM, 60);
+
+    conectarNodos(nodoL, nodoM, 50);
+
+    conectarNodos(nodoM, nodoS, 2);
+
+    conectarNodos(nodoN, nodoO, 120);
+    conectarNodos(nodoN, nodoO, 105);
+    conectarNodos(nodoN, nodoT, 60);
+    conectarNodos(nodoN, nodoS, 55);
+
+    conectarNodos(nodoO, nodoT, 195);
+
+    conectarNodos(nodoP, nodoR, 205);
+
+    conectarNodos(nodoQ, nodoR, 5);
+
+    conectarNodos(nodoR, nodoT, 230);
+
+    conectarNodos(nodoS, nodoT, 25); 
+    printf("\n--- Conexiones no dirigidas creadas ---\n");
 
     return 0;
 }
