8bf67c356a8876a73c06e10eb27d005be5b5378e
lukas <90359751+LukasMoriarty@users.noreply.github.com>
2026-04-27 11:28:37 -0700

    optimizacion interna dijkstra

--- Dijkstra.c
+++ Dijkstra.c
@@ -4,10 +4,10 @@
 #define INF 9999999
 
 typedef struct Nodo{
-	char id;			 // Identificado por una letra (A, B, C, ..., Z)
-	struct Nodo **salidas;  // Caminos que salen del nodo
-	int *pesos;			 // Lista de pesos
-	int cant;			// Cantidad de nodos conectados
+	char id;					// (A, B, C, ..., Z)
+	struct Nodo **salidas;		// Caminos que salen del nodo
+	int *pesos;					// Lista de pesos
+	int cantCaminos;			// Cantidad de nodos conectados
 } Nodo;
 
 typedef struct
@@ -24,87 +24,93 @@ void conectarNodos(Nodo* nodoA, Nodo* nodoB, int peso);
 TablaResultados *dijkstra(Nodo **nodos, int numNodos, int indiceInicio);
 
 int main() {
-    printf("--- Creando nodos ---\n");
-    Nodo *nodoA = crearNodo('A');
-    Nodo *nodoB = crearNodo('B');
-    Nodo *nodoC = crearNodo('C');
-    Nodo *nodoD = crearNodo('D');
-    Nodo *nodoE = crearNodo('E');
-    Nodo *nodoF = crearNodo('F');
-    Nodo *nodoG = crearNodo('G');
-    Nodo *nodoH = crearNodo('H');
-    Nodo *nodoI = crearNodo('I');
-    Nodo *nodoJ = crearNodo('J');
-    Nodo *nodoK = crearNodo('K');
-    Nodo *nodoL = crearNodo('L');
-    Nodo *nodoM = crearNodo('M');
-    Nodo *nodoN = crearNodo('N');
-    Nodo *nodoO = crearNodo('O');
-    Nodo *nodoP = crearNodo('P');
-    Nodo *nodoQ = crearNodo('Q');
-    Nodo *nodoR = crearNodo('R');
-    Nodo *nodoS = crearNodo('S');
-    Nodo *nodoT = crearNodo('T');
-    printf("\n--- Nodos creados ---\n");
+	printf("--- Creando nodos ---\n");
+	Nodo *nodoA = crearNodo('A');
+	Nodo *nodoB = crearNodo('B');
+	Nodo *nodoC = crearNodo('C');
+	Nodo *nodoD = crearNodo('D');
+	Nodo *nodoE = crearNodo('E');
+	Nodo *nodoF = crearNodo('F');
+	Nodo *nodoG = crearNodo('G');
+	Nodo *nodoH = crearNodo('H');
+	Nodo *nodoI = crearNodo('I');
+	Nodo *nodoJ = crearNodo('J');
+	Nodo *nodoK = crearNodo('K');
+	Nodo *nodoL = crearNodo('L');
+	Nodo *nodoM = crearNodo('M');
+	Nodo *nodoN = crearNodo('N');
+	Nodo *nodoO = crearNodo('O');
+	Nodo *nodoP = crearNodo('P');
+	Nodo *nodoQ = crearNodo('Q');
+	Nodo *nodoR = crearNodo('R');
+	Nodo *nodoS = crearNodo('S');
+	Nodo *nodoT = crearNodo('T');
+	printf("\n--- Nodos creados ---\n");
 
-    printf("\n--- Creando conexiones no dirigidas ---\n");
-    conectarNodos(nodoA, nodoB, 100);
-    conectarNodos(nodoA, nodoD, 30);
-    conectarNodos(nodoA, nodoD, 85);
+	printf("\n--- Creando conexiones no dirigidas ---\n");
+	conectarNodos(nodoA, nodoB, 100);
+	conectarNodos(nodoA, nodoD, 30);
+	conectarNodos(nodoA, nodoD, 85);
 
-    conectarNodos(nodoB, nodoC, 100);
-    conectarNodos(nodoB, nodoE, 40);
-    conectarNodos(nodoB, nodoG, 70);
-    conectarNodos(nodoB, nodoF, 45);
+	conectarNodos(nodoB, nodoC, 100);
+	conectarNodos(nodoB, nodoE, 40);
+	conectarNodos(nodoB, nodoG, 70);
+	conectarNodos(nodoB, nodoF, 45);
 
-    conectarNodos(nodoC, nodoH, 60);
-    conectarNodos(nodoC, nodoG, 55);
+	conectarNodos(nodoC, nodoH, 60);
+	conectarNodos(nodoC, nodoG, 55);
 
-    conectarNodos(nodoD, nodoP, 50);
+	conectarNodos(nodoD, nodoP, 50);
 
-    conectarNodos(nodoE, nodoI, 10);
+	conectarNodos(nodoE, nodoI, 10);
 
-    conectarNodos(nodoF, nodoJ, 55);
+	conectarNodos(nodoF, nodoJ, 55);
 
-    conectarNodos(nodoG, nodoL, 20);
-    conectarNodos(nodoG, nodoK, 15);
+	conectarNodos(nodoG, nodoL, 20);
+	conectarNodos(nodoG, nodoK, 15);
 
-    conectarNodos(nodoH, nodoL, 40);
-    conectarNodos(nodoH, nodoN, 85);
-    conectarNodos(nodoH, nodoO, 70);
+	conectarNodos(nodoH, nodoL, 40);
+	conectarNodos(nodoH, nodoN, 85);
+	conectarNodos(nodoH, nodoO, 70);
 
-    conectarNodos(nodoI, nodoP, 90);
-    conectarNodos(nodoI, nodoJ, 60);
+	conectarNodos(nodoI, nodoP, 90);
+	conectarNodos(nodoI, nodoJ, 60);
 
-    conectarNodos(nodoJ, nodoK, 80);
-    conectarNodos(nodoJ, nodoQ, 45);
+	conectarNodos(nodoJ, nodoK, 80);
+	conectarNodos(nodoJ, nodoQ, 45);
 
-    conectarNodos(nodoK, nodoM, 60);
+	conectarNodos(nodoK, nodoM, 60);
 
-    conectarNodos(nodoL, nodoM, 50);
+	conectarNodos(nodoL, nodoM, 50);
 
-    conectarNodos(nodoM, nodoS, 2);
+	conectarNodos(nodoM, nodoS, 2);
 
-    conectarNodos(nodoN, nodoO, 120);
-    conectarNodos(nodoN, nodoO, 105);
-    conectarNodos(nodoN, nodoT, 60);
-    conectarNodos(nodoN, nodoS, 55);
+	conectarNodos(nodoN, nodoO, 120);
+	conectarNodos(nodoN, nodoO, 105);
+	conectarNodos(nodoN, nodoT, 60);
+	conectarNodos(nodoN, nodoS, 55);
 
-    conectarNodos(nodoO, nodoT, 195);
+	conectarNodos(nodoO, nodoT, 195);
 
-    conectarNodos(nodoP, nodoR, 205);
+	conectarNodos(nodoP, nodoR, 205);
 
-    conectarNodos(nodoQ, nodoR, 5);
+	conectarNodos(nodoQ, nodoR, 5);
 
-    conectarNodos(nodoR, nodoT, 230);
+	conectarNodos(nodoR, nodoT, 230);
 
-    conectarNodos(nodoS, nodoT, 25);
-    printf("\n--- Conexiones no dirigidas creadas ---\n");
+	conectarNodos(nodoS, nodoT, 25);
+	printf("\n--- Conexiones no dirigidas creadas ---\n");
+
 	// crear arreglo de nodos
-	Nodo *nodos[] = {nodoA, nodoB, nodoC, nodoD, nodoE, nodoF, nodoG, nodoH, nodoI, nodoJ, nodoK, nodoL, nodoM, nodoN, nodoO, nodoP, nodoQ, nodoR, nodoS, nodoT};
+	Nodo *nodos[] = {
+		nodoA, nodoB, nodoC, nodoD, nodoE, nodoF, nodoG,
+		nodoH, nodoI, nodoJ, nodoK, nodoL, nodoM, nodoN,
+		nodoO, nodoP, nodoQ, nodoR, nodoS, nodoT
+	};
+
 	int numNodos = sizeof(nodos) / sizeof(nodos[0]);
 
-    int posNodo = 19;
+	int posNodo = 19;
 
 	TablaResultados *resultado = dijkstra(nodos, numNodos, posNodo);
 
@@ -121,14 +127,14 @@ Nodo* crearNodo(char id) {
 	nuevoNodo->id = id;
 	nuevoNodo->salidas = NULL;
 	nuevoNodo->pesos = NULL;
-	nuevoNodo->cant = 0;
+	nuevoNodo->cantCaminos = 0;
 
 	return nuevoNodo;
 }
 
 void agregarConexionUnidireccional(Nodo* nodoOriginal, Nodo* nodoSalida, int peso) {
-	nodoOriginal->cant++;
-	int indice = nodoOriginal->cant;
+	nodoOriginal->cantCaminos++;
+	int indice = nodoOriginal->cantCaminos;
 
 	Nodo **tempSalidas = (Nodo**) realloc(nodoOriginal->salidas, indice * sizeof(Nodo*));
 	if(!tempSalidas) {
@@ -147,7 +153,7 @@ void agregarConexionUnidireccional(Nodo* nodoOriginal, Nodo* nodoSalida, int pes
 	nodoOriginal->pesos[indice-1] = peso;
 }
 
-// Nueva función para establecer la conexión bidireccional (Grafo no dirigido)
+// establecer la conexión bidireccional (grafo no dirigido)
 void conectarNodos(Nodo* nodo1, Nodo* nodo2, int peso) {
 	// Se agrega la conexión en ambos sentidos
 	agregarConexionUnidireccional(nodo1, nodo2, peso);
@@ -160,8 +166,8 @@ TablaResultados *dijkstra(Nodo **nodos, int numNodos, int indiceInicio)
 	TablaResultados *resultado = (TablaResultados*) malloc(sizeof(TablaResultados));
 	resultado->numNodos = numNodos;
 
-	resultado->distancias = (int*) malloc(numNodos * sizeof(int));
-	resultado->anteriores = (int*) malloc(numNodos * sizeof(int));
+	resultado->distancias = (int*) malloc(numNodos * sizeof(int));	//reservar memoria para arreglo de distancias
+	resultado->anteriores = (int*) malloc(numNodos * sizeof(int));	//reservar memoria para el arreglo de nodos anteriores
 
 	// array tmp para nodos visitado
 	int *visitados = (int*) malloc(numNodos * sizeof(int));
@@ -171,16 +177,15 @@ TablaResultados *dijkstra(Nodo **nodos, int numNodos, int indiceInicio)
 	{
 		resultado->distancias[i] = INF;	// todos inician en INFINITO
 		resultado->anteriores[i] = -1;	// sin nodos anteriores aun
-		visitados[i] = 0;				// por defaul, ninguna esta visitado
+		visitados[i] = 0;				// por default, ninguna esta visitado
 	}
-
 	resultado->distancias[indiceInicio] = 0;
 
-	//paso 2. encontrar el camino mas corto para todos los vertices
-	for (int conteo = 0; conteo < numNodos - 1; conteo++)	// se cuentan todos los nodos menos 1, porque el ultimo se visitara automaticamente
+	//paso 2. encontrar el camino mas corto para todos los nodos
+	for (int conteo = 0; conteo < numNodos - 1; conteo++)
 	{
 		int distMinActual = INF;	// inicializar distancia mas corta
-		int nodoMin = -1;		// indice del nodo con la menor distancia (para seguir)
+		int nodoMin = -1;			// indice del nodo con la menor distancia (para seguir)
 
 		// encontrar el nodo con la distancia mas corta que no ha sido visitado
 		for (int v = 0; v < numNodos; v++)
@@ -192,15 +197,15 @@ TablaResultados *dijkstra(Nodo **nodos, int numNodos, int indiceInicio)
 				nodoMin = v;	// seteamos el indice del nuevo nodo a visitar
 			}
 		}
-		// si todos los nodos son inalcanzables, salir del loop
-		if (nodoMin == -1) break;
+		// si el nodo con menor distancia sigue siendo INF, los demás son inalcanzables
+		if (distMinActual == INF) break;
 
-		// marcar el nodo elegido como visitado para no volver a visitarlo
+		// marcar el nodo elegido como visitado
 		visitados[nodoMin] = 1;
 
 		// actualizar distancia de los nodos adyacentes del nodo elegido
 		Nodo *actual = nodos[nodoMin];
-		for (int i = 0; i < actual->cant; i++)
+		for (int i = 0; i < actual->cantCaminos; i++)
 		{
 			Nodo *adyacenteActual = actual->salidas[i];
 			int pesoAdyacente = actual->pesos[i];
@@ -215,10 +220,8 @@ TablaResultados *dijkstra(Nodo **nodos, int numNodos, int indiceInicio)
 					break;
 				}
 			}
-			if (idxDestino != -1 &&	// si encontramos el indice del primer nodo adyacente
-				!visitados[idxDestino] && // el nodo adyacente no ha sido visitado
-				resultado->distancias[nodoMin] != INF &&	// el nodo actual es alcanzable
-				resultado->distancias[nodoMin] + pesoAdyacente < resultado->distancias[idxDestino])	// la distancia al nodo destino a traves del nodo actual es menor que la distancia previamente registrada
+			// comparar distancias al nodo destino a traves del nodo actual y la distancia previamente registrada
+			if (!visitados[idxDestino] && resultado->distancias[nodoMin] + pesoAdyacente < resultado->distancias[idxDestino])
 			{
 				resultado->distancias[idxDestino] = resultado->distancias[nodoMin] + pesoAdyacente;	// actualizar la distancia al nodo adyacente
 				resultado->anteriores[idxDestino] = nodoMin;	// agregar al arreglo de anteriores el nodo actual para llegar al nodo adyacente
